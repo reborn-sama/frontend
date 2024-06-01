7b3b6991469d02792257728477cc21d6933c4f8f
GitHub Action <action@github.com>
2024-06-01 14:04:47 +0000

    Sync updates from WTF-Ethers/01_HelloVitalik

--- docs/ethers-101/01_HelloVitalik/HelloVitalik.js
+++ docs/ethers-101/01_HelloVitalik/HelloVitalik.js
@@ -1,3 +1,6 @@
+//测试workflow111213
+
+
 // 导入ethers包
 import { ethers } from "ethers";
 // playcode免费版不能安装ethers，用这条命令，需要从网络上import包（把上面这行注释掉）
@@ -5,14 +8,12 @@ import { ethers } from "ethers";
 
 // 利用ethers默认的Provider连接以太坊网络
 // const provider = new ethers.getDefaultProvider();
-const ALCHEMY_MAINNET_URL =
-  "https://eth-mainnet.g.alchemy.com/v2/oKmOQKbneVkxgHZfibs-iFhIlIAl6HDN";
-const provider = new ethers.JsonRpcProvider(ALCHEMY_MAINNET_URL);
+const ALCHEMY_MAINNET_URL = 'https://eth-mainnet.g.alchemy.com/v2/oKmOQKbneVkxgHZfibs-iFhIlIAl6HDN';
+const provider = new ethers.JsonRpcProvider(ALCHEMY_MAINNET_URL)
 
 const main = async () => {
-  // 查询vitalik的ETH余额
-  const balance = await provider.getBalance(`vitalik.eth`);
-  // 将余额输出在console
-  console.log(`ETH Balance of vitalik: ${ethers.formatEther(balance)} ETH`);
-};
-main();
+    // 查询vitalik的ETH余额
+    const balance = await provider.getBalance(`vitalik.eth`);
+    // 将余额输出在console
+    console.log(`ETH Balance of vitalik: ${ethers.formatEther(balance)} ETH`);}
+main()
